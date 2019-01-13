1daa5812085096df8d7a9b7d3bf2ed972e1e2ff4
Peter Dowling <peter.dowling9@mail.dcu.ie>
2019-01-13 10:21:10 +0000

    fixed urls.py to include / for polls link

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('polls', views.PollsView.as_view(), name='polls'),
+    path('polls/', views.PollsView.as_view(), name='polls'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
